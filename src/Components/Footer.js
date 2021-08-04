cb1b68600517e9ba90e0e739a237871f1e2933ad
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2021-08-05 04:01:22 +0530

    build finished

---
+++ src/Components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Footer = ({ data }) => {
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url} target="_blank" rel="noopener noreferrer">
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+              Made by{" "}
+              <a
+                title="KASHISH"
+                href="https://www.linkedin.com/in/kashish-singh111/"
+              >
+                KASHISH
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
